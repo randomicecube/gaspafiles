058eaeb169cc416b1d5f4a2ebd1a1dcc083c0429
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2021-07-22 19:30:12 +0100

    Add fish, flameshot, omf

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+  neofetch
+  alias compile="gcc -ansi -pedantic -Wall -Wextra -Werror"
+  alias vim="nvim"
+  alias gac="git add . && git commit -m"
+  alias gm="git checkout master"
+  alias gp="git push"
+end
