0cbb74e3a6d39f4fdae8f1b842789d8302d71121
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2021-07-27 23:22:18 +0100

    Add wallpapers + clean .xinitrc

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,8 @@
 if status is-interactive
 	neofetch
+	export GTK_THEME=Dracula
+	export EDITOR=nvim
+	export PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/lib/jvm/default/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:.local/bin
   alias compile="gcc -ansi -pedantic -Wall -Wextra -Werror"
   alias vim="nvim"
   alias gac="git add . && git commit -m"
