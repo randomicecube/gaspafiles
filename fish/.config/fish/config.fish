b312080f38d445f993067d8abe2b330fa1452dda
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2021-10-03 16:59:32 +0100

    Change fish config + dunst

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,6 @@ if status is-interactive
 	export GTK_THEME=Dracula
 	export EDITOR=nvim
 	export PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/lib/jvm/default/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:.local/bin
-  alias compile="gcc -ansi -pedantic -Wall -Wextra -Werror"
   alias vim="nvim"
 	alias neo="neofetch"
   alias gac="git add . && git commit -m"
