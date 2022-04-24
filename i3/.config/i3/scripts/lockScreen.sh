b8976c32dd542375aba1fda0f06266f9c3981826
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-04-24 23:28:43 +0100

    spring cleaning (with more to come

---
+++ i3/.config/i3/scripts/lockScreen.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+greet="wassup"
+correct="yey!"
+color="ffffff"
+wrong_pass="tentaste"
+font="JetBrains Mono"
+background="$HOME/Pictures/peace.jpg"
+
+i3lock \
+  -t \
+  --nofork \
+  --ignore-empty-password \
+  --indicator \
+  --bar-indicator \
+  --bar-pos="1070" \
+  --bar-base-width="10" \
+  --bar-orientation=horizontal \
+  --bar-color=151515 \
+  --ringver-color=$color \
+  --ringwrong-color=$color \
+  --bshl-color=$color \
+  --keyhl-color=$color \
+  --clock \
+  --time-color=$color \
+  --time-str="%H:%M:%S" \
+  --time-font="$font" \
+  --time-size=72 \
+  --time-color=$color \
+  --date-color=$color \
+  --date-str="%A, %d %B" \
+  --date-color=$color \
+  --date-font="$font" \
+  --date-size=18 \
+  --greeter-text=$greet \
+  --greeter-color=$color \
+  --greeter-font="$font" \
+  --verif-font="$font" \
+  --verif-size=24 \
+  --verif-text="$correct" \
+  --verif-color=$color\
+  --wrong-font="$font" \
+  --wrong-size=24 \
+  --wrong-text="$wrong_pass" \
+  --wrong-color=$color \
+  -i $background
---
+++ i3/.config/i3/scripts/wacom.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+xsetwacom set "Wacom One by Wacom M Pen stylus" MapToOutput DisplayPort-0
