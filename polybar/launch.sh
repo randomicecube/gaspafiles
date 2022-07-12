dabfadb6a9ddde1fe8809abc4dd080d9437557ba
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-07-12 14:22:19 +0100

    update polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,10 +2,5 @@
 
 killall -q polybar
 
-polybar i3 -c ~/.config/polybar/config &
-polybar i3_m2 -c ~/.config/polybar/config &
-
-#polybar space -c ~/.config/polybar/workspace.ini &
-#polybar primary -c ~/.config/polybar/workspace.ini &
-#polybar top -c ~/.config/polybar/current.ini &
-#polybar clock -c ~/.config/polybar/current.ini &
+polybar main -c ~/.config/polybar/config &
+polybar aux -c ~/.config/polybar/config &
--- polybar/scripts/prev_spotify_music.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-PLAYER="spotify"
-playerctl --player=$PLAYER previous
