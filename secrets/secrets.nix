11e97398c4fd4c203318b75fbc5876f69c479b1e
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2023-02-14 22:05:45 +0000

    Add clockwerk system key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   bentleySystem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF/4tWBjIa9ovRVWxHbOdiFcnLI+HsvEbFZFM4re+T/c gaspa@bentley";
-  clockwerkSystem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFgZUGgluhhI6Pg+LCp70WCy2YX12of8Cc6cO6JQJDzy gaspa@clockwerk";
+  clockwerkSystem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK3UGLE0qEeVJasmzNaPHw5VsqNoUyjxTjO9JZL95UhA";
   slySystem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH1z3pPqbGtSLABFGKYrSbZ1wq+j8e8W9Nobyn1yrhB7";
 in {
   # add stuff here
