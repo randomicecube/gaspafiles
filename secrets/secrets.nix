a44352ef33b2365fc8834fa6e4ace32a24c8d0ff
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2023-02-14 22:08:01 +0000

    Add bentley system key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  bentleySystem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF/4tWBjIa9ovRVWxHbOdiFcnLI+HsvEbFZFM4re+T/c gaspa@bentley";
+  bentleySystem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH/CWZqht1x67lb5efuR+zElF/VqpC95pr+s1P3plqn4";
   clockwerkSystem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK3UGLE0qEeVJasmzNaPHw5VsqNoUyjxTjO9JZL95UhA";
   slySystem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH1z3pPqbGtSLABFGKYrSbZ1wq+j8e8W9Nobyn1yrhB7";
 in {
