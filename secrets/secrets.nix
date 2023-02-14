58a43ef32361f87a0c1266557601db2e5261742e
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2023-02-14 22:03:43 +0000

    Add sly system key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   bentleySystem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF/4tWBjIa9ovRVWxHbOdiFcnLI+HsvEbFZFM4re+T/c gaspa@bentley";
   clockwerkSystem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFgZUGgluhhI6Pg+LCp70WCy2YX12of8Cc6cO6JQJDzy gaspa@clockwerk";
-  slySystem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIPXnaZy4Pk+3qL6dx4iBnPCLTpGgf8yzhkPe1AcR+/K gaspa@sly";
+  slySystem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH1z3pPqbGtSLABFGKYrSbZ1wq+j8e8W9Nobyn1yrhB7";
 in {
   # add stuff here
 };
