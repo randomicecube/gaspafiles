817cdb336b99f23fbbfaa1a871d8be010113f55a
Diogo Gaspar <diogo@gaspa.pt>
2023-07-18 00:17:54 +0100

    modules/graphical/zathura: change dark theme to rose pine moon

--- modules/graphical/zathura.nix
+++ modules/graphical/zathura.nix
@@ -15,8 +15,48 @@ in {
       options = {
         selection-clipboard = "clipboard";
         recolor = "true"; # open documents with inverted colors by default
+        recolor-keephue = "true";
         sandbox = "none"; # fix links not opening on browser
       };
+      extraConfig = ''
+        # Change 'recolor' and 'recolor-keephue' to true to change
+        # the document colors for a more uniform viewing experience.
+
+        # This is the Rosé Pine Moon theme!
+
+        set default-bg                  "#232136"
+        set default-fg                  "#e0def4"
+
+        set statusbar-fg                "#e0def4"
+        set statusbar-bg                "#59546d"
+
+        set inputbar-bg                 "#817c9c"
+        set inputbar-fg                 "#232136"
+
+        set notification-bg             "#817c9c"
+        set notification-fg             "#232136"
+
+        set notification-error-bg       "#817c9c"
+        set notification-error-fg       "#ea9a97"
+
+        set notification-warning-bg     "#817c9c"
+        set notification-warning-fg     "#f6c177"
+
+        set highlight-color             "#3e8fb0"
+        set highlight-active-color      "#9ccfd8"
+
+        set completion-bg               "#817c9c"
+        set completion-fg               "#9ccfd8"
+
+        set completion-highlight-fg     "#e0def4"
+        set completion-highlight-bg     "#9ccfd8"
+
+        set recolor-lightcolor          "#232136"
+        set recolor-darkcolor           "#e0def4"
+
+        set recolor                     "false"
+        set recolor-keephue             "false"
+      '';
     };
 
     xdg.mimeApps = {
