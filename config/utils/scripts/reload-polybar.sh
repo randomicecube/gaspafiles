91b33900bde727ec35b4f7d2bd5d211fbfdc2d54
Diogo Gaspar <diogo@gaspa.pt>
2023-07-20 11:11:20 +0100

    config/utils/scripts/reload_polybar: always do everything

--- config/utils/scripts/reload-polybar.sh
+++ config/utils/scripts/reload-polybar.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-if pgrep "polybar" > /dev/null; then
-    pkill -USR1 polybar
-else
-    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload bar&
-    done
-fi
+pkill -USR1 polybar
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bar&
+done
 
