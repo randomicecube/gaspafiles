6bceb196f5b547c523a4dc8606e8b726abb9df3b
Diogo Gaspar <diogo@gaspa.pt>
2025-02-18 14:15:13 +0100

    remove IST kerberos stuff

--- modules/personal.nix
+++ modules/personal.nix
@@ -57,28 +57,6 @@ in {
       };
     };
 
-    # to login into Fenix with Kerberos, on Firefox's about:config
-    # network.negotiate-auth.trusted-uris	= id.tecnico.ulisboa.pt
-    krb5 =  {
-      enable = true;
-      libdefaults = {
-        default_realm = "IST.UTL.PT";
-        kdc_timesync = 1;
-        ccache_type = 4;
-        forwardable = true;
-        proxiable = true;
-      };
-      domain_realm = {
-        "ist.utl.pt" = "IST.UTL.PT";
-        ".ist.utl.pt" = "IST.UTL.PT";
-      };
-      realms = {
-        "IST.UTL.PT" = {
-          admin_server = "kerberosmaster.ist.utl.pt";
-        };
-      };
-    };
-
     # Printer
     services.printing.enable = true;
     services.printing.drivers = with pkgs; [
